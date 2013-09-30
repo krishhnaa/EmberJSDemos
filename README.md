deec4ab595b0010a21e01ff184c3d20585193900
krishna <Krishna@Krishnas-MacBook-Pro.local>
2013-09-29 22:37:46 -0400

    Added twoway binding html page

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ EmberJS demo is the project of series of applications usins Emberjs
 1) Display list of data
 
 2)Displaying the date with links and navigate the user with the links
+
+3) Explaing Twoway binding by editing the user and saving..
--- js/app.js
+++ js/app.js
@@ -2,12 +2,7 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   this.resource('user', {path: '/users/:user_id'});
-});
-
-App.UserRoute = Ember.Route.extend({
-  model: function(params) {
-    return users[params.user_id];
-  }
+  this.resource('editUser', {path: '/users/:user_id/edit'});
 });
 
 App.ApplicationRoute = Ember.Route.extend({
@@ -16,21 +11,45 @@ App.ApplicationRoute = Ember.Route.extend({
   }
 });
 
+App.UserRoute = Ember.Route.extend({
+  model: function(params) {
+    return users[params.user_id];
+  }
+});
+
+App.EditUserRoute = Ember.Route.extend({
+  model: function(params) {
+    return users[params.user_id];
+  },
+  
+  events: {
+    saveuser: function() {
+      var user = this.modelFor('editUser');
+      this.transitionTo('user', user);
+    }
+  }
+});
+
+
+
+
+
+
 var users = [
   {
-    id: 1,
+    id: 0,
     first: 'Ryan',
     last: 'Florence',
     avatar: 'https://si0.twimg.com/profile_images/3123276865/5c069e64eb7f8e971d36a4540ed7cab2.jpeg'
   },
   {
-    id: 2,
+    id: 1,
     first: 'Tom',
     last: 'Dale',
     avatar: 'https://si0.twimg.com/profile_images/1317834118/avatar.png'
   },
   {
-    id: 3,
+    id: 2,
     first: 'Yehuda',
     last: 'Katz',
     avatar: 'https://si0.twimg.com/profile_images/3250074047/46d910af94e25187832cb4a3bc84b2b5.jpeg'
